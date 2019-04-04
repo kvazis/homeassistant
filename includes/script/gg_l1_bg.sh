daa46619d3b3257557952b60e844d12f9dcc7654
Kvazis <greatkvazis@gmail.com>
2019-04-04 11:18:24 +0000

    0.91 yeelight platform

--- includes/script/gg_l1_bg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-echo -ne '{"id": 1, "method":"bg_set_scene", "params":["color", 1315890, 50]}\r\n' | nc -w1 192.168.0.148 55443
-echo -ne '{"id": 1, "method":"bg_set_scene", "params":["color", 1315890, 50]}\r\n' | nc -w1 192.168.0.166 55443
-echo -ne '{"id": 1, "method":"set_scene", "params":["color", 1315890, 50]}\r\n' | nc -w1 192.168.0.212 55443
-echo -ne '{"id": 1, "method":"set_scene", "params":["color", 1315890, 50]}\r\n' | nc -w1 192.168.0.214 55443
